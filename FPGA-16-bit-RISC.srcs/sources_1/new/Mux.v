88c6e3e325a8e77b826806a681129652a52d707c
ch1pset <theslenderpsycho@gmail.com>
2019-08-04 15:28:02 -0500

    Added sign extension to 3x1 Mux

--- FPGA-16-bit-RISC.srcs/sources_1/new/Mux.v
+++ FPGA-16-bit-RISC.srcs/sources_1/new/Mux.v
@@ -19,6 +19,18 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module Mux16_3x1(
+    input [15:0] bus [2:0],
+    input [1:0] sel,
+    output [15:0] dout
+    );
+    wire [15:0] sign_extend;
+    assign sign_extend[15] = bus[2][7];
+    assign sign_extend[14:7] = 8'h00;
+    assign sign_extend[6:0] = (~bus[2][6:0]) + 1;
+
+    Mux #(16, 4) M0({0, sign_extend, bus[1], bus[0]}, sel, dout);
+endmodule
 
 module Mux(bus, sel, dout);
     parameter WIDTH = 8, SIZE = 16;
@@ -29,4 +41,3 @@ module Mux(bus, sel, dout);
 
     assign dout = bus[sel];
 endmodule
-
