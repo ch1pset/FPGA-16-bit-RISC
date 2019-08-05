6518a0bd108860f750938cd2d30016cbe8e9e1b8
ch1pset <theslenderpsycho@gmail.com>
2019-08-04 22:37:00 -0500

    Added RISC_16bit top level module

---
+++ FPGA-16-bit-RISC.srcs/sources_1/new/RISC_16bit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/04/2019 10:32:50 PM
+// Design Name: 
+// Module Name: RISC_16bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RISC_16bit(
+    
+    );
+endmodule
