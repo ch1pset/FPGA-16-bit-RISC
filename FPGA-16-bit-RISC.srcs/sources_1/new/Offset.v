b1e250448b6193b96cfbd5994de8a273cb35487a
ch1pset <theslenderpsycho@gmail.com>
2019-08-04 22:35:45 -0500

    Added Offset_sel for direct PC load or offset calc

--- FPGA-16-bit-RISC.srcs/sources_1/new/Offset.v
+++ FPGA-16-bit-RISC.srcs/sources_1/new/Offset.v
@@ -21,12 +21,16 @@
 
 
 module Offset(
-    input [7:0] instr_ptr,
+    input [7:0] instr_ptr, instr_load,
     input [7:0] offset,
+    input sel,
     output [7:0] instr_off_ptr
     );
     wire [7:0] pos, neg;
     assign pos = instr_ptr + offset[6:0] - 1;
     assign neg = instr_ptr - offset[6:0] - 1;
-    assign instr_off_ptr = offset[7] ? neg : pos;
+
+    assign instr_off_ptr =  sel ? 
+                            offset[7] ? neg : pos
+                            : instr_load;
 endmodule
