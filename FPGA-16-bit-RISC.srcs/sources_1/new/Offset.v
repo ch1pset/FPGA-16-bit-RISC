b35239b8cf6cd2e4f92db73475868a0811fc5027
ch1pset <theslenderpsycho@gmail.com>
2019-08-05 02:34:24 -0500

    Updated Offset module

--- FPGA-16-bit-RISC.srcs/sources_1/new/Offset.v
+++ FPGA-16-bit-RISC.srcs/sources_1/new/Offset.v
@@ -21,16 +21,18 @@
 
 
 module Offset(
-    input [7:0] instr_ptr, instr_load,
-    input [7:0] offset,
-    input sel,
+    input [7:0] instr_ptr,
+    input [7:0] offset_addr,
     output [7:0] instr_off_ptr
     );
-    wire [7:0] pos, neg;
-    assign pos = instr_ptr + offset[6:0] - 1;
-    assign neg = instr_ptr - offset[6:0] - 1;
+    wire [7:0] offset;
+    wire sign = (offset_addr > instr_ptr);
 
-    assign instr_off_ptr =  sel ? 
-                            offset[7] ? neg : pos
-                            : instr_load;
+    assign offset = sign ? 
+        (offset_addr - instr_ptr): 
+        (instr_ptr - offset_addr);
+
+    assign instr_off_ptr = sign ?
+        (instr_ptr + offset - 1):
+        (instr_ptr - offset - 1);
 endmodule
