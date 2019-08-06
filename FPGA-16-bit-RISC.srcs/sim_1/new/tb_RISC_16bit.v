0f00dcb669e1b4e4aa4fba33027133318cc07eec
ch1pset <theslenderpsycho@gmail.com>
2019-08-06 01:23:47 -0500

    Adjusted tb_RISC_16bit

--- FPGA-16-bit-RISC.srcs/sim_1/new/tb_RISC_16bit.v
+++ FPGA-16-bit-RISC.srcs/sim_1/new/tb_RISC_16bit.v
@@ -21,30 +21,25 @@
 
 
 module tb_RISC_16bit( );
-    reg [15:0] Instr;
+    reg [15:0] Instr = 0, Rs_out = 0;
     reg Rs_zero = 0, clk = 1, rst = 0;
-    wire [7:0] Rd_data;
+    wire [7:0] Rd_data, PC_addr;
     wire [3:0] Rd_addr, Rs_addr, Rt_addr, Opcode;
     wire [2:0] ALU_sel;
     wire [1:0] Rd_sel;
     wire Rs_sel;
     wire Rd_write, Rs_read, Rt_read;
-    wire PC_load, PC_clr, PC_inc;
-    wire IR_sel, IR_load;
     wire Mem_sel, Mem_read, Mem_write;
 
     assign Opcode = Instr[15:12];
     
-    Controller UUT1(
-        Instr,
+    ControlUnit UUT1(
+        Instr, Rs_out,
         Rs_zero, clk, rst,
-        Rd_data,
+        Rd_data, PC_addr,
+        ALU_sel, Rd_sel, Rs_sel,
         Rd_addr, Rs_addr, Rt_addr,
-        ALU_sel,
-        Rd_sel, Rs_sel,
         Rd_write, Rs_read, Rt_read,
-        PC_load, PC_clr, PC_inc,
-        IR_sel, IR_load,
         Mem_sel, Mem_read, Mem_write
     );
 
@@ -52,52 +47,35 @@ module tb_RISC_16bit( );
     begin
         Instr = 16'h0201;
         #1 rst = 1;
-
         nextInstr(16'h1201);
         toggleRst();
-
         nextInstr(16'h2201);
         toggleRst();
-
         nextInstr(16'h3201);
         toggleRst();
-
         nextInstr(16'h4201);
         toggleRst();
-
         nextInstr(16'h5300);
         toggleRst();
-
         nextInstr(16'h6300);
         toggleRst();
-
         nextInstr(16'h7300);
         toggleRst();
-
         nextInstr(16'h84ab);
         toggleRst();
-
         nextInstr(16'h94a0);
         toggleRst();
-
         nextInstr(16'ha5a0);
         toggleRst();
-
         nextInstr(16'hb501);
         toggleRst();
-
         nextInstr(16'hc501);
         toggleRst();
-
         nextInstr(16'hd6a0);
         toggleRst();
-
         nextInstr(16'he0a0);
         toggleRst();
-
-        nextInstr(16'hf0a0);
-        toggleRst();
-        
+        nextInstr(16'hf0a0);        
         #320 $finish;
     end
 
@@ -106,7 +84,7 @@ module tb_RISC_16bit( );
     task nextInstr;
     input [15:0] word;
     begin
-        #18 Instr = word;
+        #24 Instr = word;
     end
     endtask
 
